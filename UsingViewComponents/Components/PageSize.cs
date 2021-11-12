96c2aa812ffe3f0a8dc2f71866bb6db0d76f3689
UnName <UnName@DESKTOP-UGBR7VT>
2021-11-12 13:11:56 +0400

    Добавьте файлы проекта.

---
+++ UsingViewComponents/Components/PageSize.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UsingViewComponents.Components
+{
+    public class PageSize : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("http://apress.com");
+            return View(responseMessage.Content.Headers.ContentLength);
+        }
+    }
+}
---
+++ UsingViewComponents/Controllers/CityController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Controllers
+{
+    [ViewComponent(Name = "ComboComponent")]
+    public class CityController : Controller
+    {
+        private ICityRepository repository;
+
+        public CityController(ICityRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Create() => View();
+        [HttpPost]
+        public IActionResult Create(City newCity) 
+        {
+            repository.AddCity(newCity);
+            return RedirectToAction("Index","Home");
+        }
+        public IViewComponentResult Invoke() 
+        {
+            return new ViewViewComponentResult() 
+            {
+                ViewData = new Microsoft.AspNetCore.Mvc.ViewFeatures.ViewDataDictionary<IEnumerable<City>>(ViewData, repository.Cities)
+            }; 
+        }
+    }
+}
